b05164b05c3795e6fde762d4f289df00f1e599ab
Yogesh Nandanwar <yogesh.nandanwar007.yn16@gmail.com>
2025-02-08 21:05:47 +0530

    frist comit

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const codeInput = document.getElementById('code');
 const submitBtn = document.getElementById('submit-btn');
 const resultDiv = document.getElementById('result');
 
-const secretCode = 'pilludi'; // Replace with your secret code
+const secretCode = 'anagha'; // Replace with your secret code
 const nextPageUrl = 'main.html'; // Replace with the URL of the new page
 
 form.addEventListener('submit', (e) => {
